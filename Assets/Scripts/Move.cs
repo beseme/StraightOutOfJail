7ab994ff569c2db27996312c5131fd71f89916ce
Bennet Meier <bennet.meier@gmx.net>
2019-11-01 20:56:01 +0100

    new movement

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -4,19 +4,35 @@ using UnityEngine;
 
 public class Move : MonoBehaviour
 {
+    [SerializeField]
+    private Sprite[] _sprite = new Sprite[2];
+    private int _spriteIndex = 0;
+    private bool _flipped;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _flipped = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.A))        
-            gameObject.transform.position += new Vector3(-2, 0, 0);
-        if (Input.GetKeyDown(KeyCode.D))
-            gameObject.transform.position += new Vector3(2, 0, 0);
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            _flipped = !_flipped;
+            if (_flipped)
+                GetComponent<SpriteRenderer>().sprite = _sprite[1];
+            if (!_flipped)
+                GetComponent<SpriteRenderer>().sprite = _sprite[0];
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if(_flipped)
+                gameObject.transform.position += new Vector3(-2, 0, 0);
+            else
+                gameObject.transform.position += new Vector3(2, 0, 0);
+        }
 
     }
 }
