b0b4835eac68dff4f825f52816baaf083a18e4e6
Unknown <beyerle_lukas@t-online.de>
2019-11-01 21:44:33 +0100

    teleporter
    anfang

---
+++ Assets/Scripts/teleport.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class teleport : MonoBehaviour
+{
+    [SerializeField]
+    public GameObject _player;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        _player.transform.position = new Vector3(_player.transform.position.x, _player.transform.position.y, -6);
+    }
+}
