e6738e01ee202971bd5db94139bd76ad181aac92
Bennet Meier <bennet.meier@gmx.net>
2019-11-01 21:11:58 +0100

    UI script

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -8,11 +8,14 @@ public class Move : MonoBehaviour
     private Sprite[] _sprite = new Sprite[2];
     private int _spriteIndex = 0;
     private bool _flipped;
+    private float _stamina;
+    public UIBar _bar;
 
     // Start is called before the first frame update
     void Start()
     {
         _flipped = false;
+        _stamina = 100;
     }
 
     // Update is called once per frame
@@ -32,7 +35,9 @@ public class Move : MonoBehaviour
                 gameObject.transform.position += new Vector3(-2, 0, 0);
             else
                 gameObject.transform.position += new Vector3(2, 0, 0);
+            _stamina -= 5;
         }
 
+        _bar.GetComponent<UIBar>().Bar(_stamina, 1, 100, 0, 1);
     }
 }
---
+++ Assets/Scripts/UIBar.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIBar : MonoBehaviour
+{
+    [SerializeField]
+    private Image _staminaBar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _staminaBar.fillAmount = 1; 
+    }
+
+    private float Map(float value, float inMin, float inMax, float outMin, float outMax)
+    {
+        return (value - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+    }
+
+    public void Bar(float value, float inMin, float inMax, float outMin, float outMax)
+    {
+        _staminaBar.fillAmount = Map(value, inMin, inMax, outMin, outMax);
+    }
+
+}
