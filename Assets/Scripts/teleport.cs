b21a484ccab8fcfeb2e36f69c85d1fe6099f6e0c
Bennet Meier <bennet.meier@gmx.net>
2019-11-02 13:04:52 +0100

    Teleport repair

--- Assets/Scripts/teleport.cs
+++ Assets/Scripts/teleport.cs
@@ -4,17 +4,11 @@ using UnityEngine;
 
 public class teleport : MonoBehaviour
 {
-    [SerializeField]
-    public GameObject _player;
-
-    // Start is called before the first frame update
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        _player.transform.position = new Vector3(_player.transform.position.x, _player.transform.position.y - 6, 0);
+        if(collision.gameObject.tag == "Elevator")
+        gameObject.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y - 6, 0);
     }
-    // Update is called once per frame
-
-
 
 }
