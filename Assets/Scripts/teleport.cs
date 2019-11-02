05215c7477c63131613d8c4ff6cd2aa8ce5c268f
Unknown <beyerle_lukas@t-online.de>
2019-11-02 19:10:39 +0100

    teleport sound

--- Assets/Scripts/teleport.cs
+++ Assets/Scripts/teleport.cs
@@ -4,10 +4,27 @@ using UnityEngine;
 
 public class teleport : MonoBehaviour
 {
+    private AudioSource _sound;
+    public AudioClip _clip;
 
+    private void Start()
+    {
+        _sound = GetComponent<AudioSource>();
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.gameObject.tag == "Elevator")
+        if (collision.gameObject.tag == "Elevator")
+        {
+
+            StartCoroutine(Emu());
+
+        }
+    }
+
+    private IEnumerator Emu()
+    {
+        _sound.PlayOneShot(_clip, 1);
+        yield return new WaitForSeconds(_clip.length * 0.5f);
         gameObject.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y - 6, 0);
     }
 
