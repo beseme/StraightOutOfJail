0fc29a6861ded71c5b03317fb52424bfbb331c58
Unknown <beyerle_lukas@t-online.de>
2019-11-02 12:54:08 +0100

    Teleport

--- Assets/Scripts/teleport.cs
+++ Assets/Scripts/teleport.cs
@@ -6,17 +6,15 @@ public class teleport : MonoBehaviour
 {
     [SerializeField]
     public GameObject _player;
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    // Start is called before the first frame update
+
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
-        _player.transform.position = new Vector3(_player.transform.position.x, _player.transform.position.y, -6);
+        _player.transform.position = new Vector3(_player.transform.position.x, _player.transform.position.y - 6, 0);
     }
+    // Update is called once per frame
+
+
+
 }
