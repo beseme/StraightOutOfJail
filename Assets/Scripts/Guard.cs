f8d0b0bd771116a3eccac5bce46e0ee2a153780e
Bennet Meier <bennet.meier@gmx.net>
2019-11-02 12:41:13 +0100

    Fail Screen

--- Assets/Scripts/Guard.cs
+++ Assets/Scripts/Guard.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Guard : MonoBehaviour
 {
@@ -19,6 +20,10 @@ public class Guard : MonoBehaviour
     private Sprite[] _spriteFlip = new Sprite[2];
     private int _spriteIndex = 0;
     private LineRenderer _view;
+    [SerializeField]
+    private Image _fail;
+    public Move Player;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -27,6 +32,7 @@ public class Guard : MonoBehaviour
         _view = GetComponent<LineRenderer>();
         _points = new Vector3[2];
         _lineDirection = new Vector3(-LineLength, 0, 0);
+        _fail.gameObject.SetActive(false);
     }
 
     void Flip()
@@ -54,6 +60,12 @@ public class Guard : MonoBehaviour
 
         _hit = Physics2D.Raycast(_rayOrigin.position, _rayDirection, 5f, LayerMask.GetMask("Player"));
         _wallHit = Physics2D.Raycast(_rayOrigin.position, _rayDirection, 4f, LayerMask.GetMask("Wall"));
+
+        if (_hit && Player._hidden == false)
+        {
+            _fail.gameObject.SetActive(true);
+            Time.timeScale = 0;
+        }
 
         if (_wallHit)
             Flip();
